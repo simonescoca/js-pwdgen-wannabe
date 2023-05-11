fd24c751702f8f3140951071d6f3a5e424645bff
simonescoca <simone.scoca@icloud.com>
2023-05-11 14:47:23 +0200

    added surname

--- myscript.js
+++ myscript.js
@@ -4,5 +4,6 @@
 // Infine scrivi sulla pagina nomecognomecolorepreferito21
 
 const nome = prompt ( "Qual è il tuo nome?" )
+const cognome = prompt ( "Qual è il tuo cognome?" )
 const colorePreferito = prompt ( "Qual è il tuo colore preferito?" )
-document.getElementById ( "pw" ) .innerHTML = nome + colorePreferito + "21"
+document.getElementById ( "pw" ) .innerHTML = nome + cognome + colorePreferito + "21"
